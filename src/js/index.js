e0f6130046d918241e1af58bcee13bf937716211
InkInn <lrc19931109@outlook.com>
2017-10-22 21:47:29 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,22 @@ var app = new Vue({
             name: '',
             password: ''
           }
-    }
+    },
+    methods: {
+        login() {
+          this.$notify({
+            title: '登录成功',
+            message: '这是一条成功的登录消息',
+            type: 'success'
+          });
+        },
+  
+        register() {
+          this.$notify({
+            title: '注册成功',
+            message: '这是一条成功的注册消息',
+            type: 'success'
+          });
+        }
+      }
 })
